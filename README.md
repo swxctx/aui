6a5747207e3e816896d62bed16b8aa4a5c284312
swxctx <swxctx@gmail.com>
2024-04-18 18:05:50 +0800

    add 打字机效果

--- README.md
+++ README.md
@@ -30,3 +30,23 @@ implementation 'com.github.swxctx:aui:v0.0.1'
         tools:textSize="14sp" />
 </com.swxctx.aui.MoveFrameLayout>
 ```
+
+- 打字机效果
+
+```
+<com.swxctx.aui.PrinterTextView
+    android:id="@+id/p_tv"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"
+    android:layout_marginHorizontal="20dp"
+    android:layout_marginTop="20dp"
+    android:textColor="@color/white"/>
+
+private PrinterTextView printerTextView;
+printerTextView = findViewById(R.id.p_tv);
+
+// 设置打字文本
+printerTextView.setPrintText("安卓（Android）是一种基于Linux内核（不包含GNU组件）的自由及开放源代码的移动操作系统。主要应用于移动设备，如智能手机和平板电脑，由美国Google公司和开放手机联盟领导及开发。");
+// 开始打字
+printerTextView.startPrint();
+```
--- app/src/main/java/com/swxctx/demo/MainActivity.java
+++ app/src/main/java/com/swxctx/demo/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.swxctx.aui.PrinterTextView;
+
 public class MainActivity extends AppCompatActivity {
+    private PrinterTextView printerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initData();
+    }
+
+    private void initView(){
+        printerTextView = findViewById(R.id.p_tv);
+    }
+
+    private void initData(){
+        // 设置打字文本
+        printerTextView.setPrintText("安卓（Android）是一种基于Linux内核（不包含GNU组件）的自由及开放源代码的移动操作系统。主要应用于移动设备，如智能手机和平板电脑，由美国Google公司和开放手机联盟领导及开发。");
+        // 开始打字
+        printerTextView.startPrint();
     }
 }
